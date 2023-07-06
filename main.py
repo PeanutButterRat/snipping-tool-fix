5140e0b9cd05a56abba8bf596845ae3ce8ba2e7d
Eric Brown <ebrown5676@gmail.com>
2023-07-06 13:47:37 -0700

    Added a shortcut to terminate the script

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import subprocess
+import os
+import signal
 
 import keyboard
 
 
 SHORTCUT = 'windows+shift+s'
 LOCATION = 'C:\\Windows\\system32\\SnippingTool.exe'
+QUIT = 'ctrl+shift+s'
 
 
 def main():
     keyboard.add_hotkey(SHORTCUT, execute)
+    keyboard.add_hotkey(QUIT, os.kill, (os.getpid(), signal.SIGTERM))
     keyboard.wait()
 
 
